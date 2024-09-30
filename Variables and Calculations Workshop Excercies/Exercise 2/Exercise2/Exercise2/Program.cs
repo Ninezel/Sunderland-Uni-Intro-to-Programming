ce68c0634b44e4a3be560db0ec69c2bd7242f445
Ninezel <36775518+Ninezel@users.noreply.github.com>
2024-09-30 12:59:57 +0100

    Update

--- Variables and Calculations Workshop Excercies/Exercise 2/Exercise2/Exercise2/Program.cs
+++ Variables and Calculations Workshop Excercies/Exercise 2/Exercise2/Exercise2/Program.cs
@@ -10,28 +10,28 @@ namespace Exercise2
     {
         static void Main(string[] args)
         {
-            // 
+            // We are Declaring String Variables Here
             String sName, sProgramme;
 
-            //
+            // We are calling on the Console to Allow the User to Input into the Console
             Console.Write("Enter your name: ");
 
-            //
+            // Whatever the user input into the console it will be saved into the string sName variable
             sName = Console.ReadLine();
 
-            //
+            // We are calling on the console again to get information from the user
             Console.Write("Which programme are you studying: ");
 
-            //
+            // We are storing that value into the sProgramme string
             sProgramme = Console.ReadLine();
 
-            //
+            // We are now outputting to the console the answer that we have gathered
             Console.WriteLine("Hello " + sName + " from " + sProgramme);
 
-            //
+            // We are notifying the user that they can press any key to close the console
             Console.WriteLine("Press any key to close.");
 
-            //
+            // This is the end of the code, any key pressed by the user will close the console
             Console.ReadKey();
 
         }
