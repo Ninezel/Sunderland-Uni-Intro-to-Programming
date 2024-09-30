ff0843803ceb34cce19a435f86190bd4dde254a7
Ninezel <36775518+Ninezel@users.noreply.github.com>
2024-09-30 12:54:48 +0100

    Update and Cleanup

--- Variables and Calculations Workshop Excercies/Exercise 1/EmailSignature/EmailSignature/Program.cs
+++ Variables and Calculations Workshop Excercies/Exercise 1/EmailSignature/EmailSignature/Program.cs
@@ -15,6 +15,9 @@ namespace EmailSignature
             const string sADDRESS = "David Goldman Informatics Centre St Peter's Way \nSunderland SR6 0DD";
             const string sFACULTY = "Faculty of Technology";
 
+            // Variables
+            String sStaffName, sRoomNumber, sStaffEmail;
+
             // The Beginning of the Program that will make the console wait 5 seconds
             Console.Write("Email Signature Creation Software");
             Thread.Sleep(5000);
@@ -22,18 +25,18 @@ namespace EmailSignature
 
             // User input declaring the variables
             Console.Write("Please Input Your Full Name: ");
-            string sStaffName = Console.ReadLine();
+            sStaffName = Console.ReadLine();
             Console.Write("Please Enter the Room Number: ");
-            string sRoomNumber = Console.ReadLine();
+            sRoomNumber = Console.ReadLine();
             Console.Write("Please Enter Your Email Address: ");
-            string sStaffEmail = Console.ReadLine();
+            sStaffEmail = Console.ReadLine();
 
             // User output of results
             Console.Clear();
             Console.WriteLine(sStaffName);
             Console.WriteLine(" ");
             Console.WriteLine(sFACULTY);
-            Console.WriteLine(sRoomNumber);
+            Console.WriteLine("Room Number: " + sRoomNumber);
             Console.WriteLine(sADDRESS);
             Console.WriteLine(" ");
             Console.WriteLine(sStaffEmail);
---
+++ Variables and Calculations Workshop Excercies/Exercise 2/Exercise2/Exercise2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 
+            String sName, sProgramme;
+
+            //
+            Console.Write("Enter your name: ");
+
+            //
+            sName = Console.ReadLine();
+
+            //
+            Console.Write("Which programme are you studying: ");
+
+            //
+            sProgramme = Console.ReadLine();
+
+            //
+            Console.WriteLine("Hello " + sName + " from " + sProgramme);
+
+            //
+            Console.WriteLine("Press any key to close.");
+
+            //
+            Console.ReadKey();
+
+        }
+    }
+}
